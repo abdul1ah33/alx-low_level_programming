a960d5dbcad0ec360bd70ed93e874059ecc8c1f1
abdul1ah33 <study.abdullah2005@gmail.com>
2023-08-10 15:49:04 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Description: A C program that prints with printf function
+ * Description: A C program that prints with printf function.
  *
  * Return: Always 0 (Success)
 */
