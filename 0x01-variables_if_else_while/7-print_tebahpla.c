71d49be363f713e5b24c4f5a4871936c46dbeb92
abdul1ah33 <study.abdullah2005@gmail.com>
2023-08-12 08:54:14 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print all alphabet letters in lowercase then uppercase
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
