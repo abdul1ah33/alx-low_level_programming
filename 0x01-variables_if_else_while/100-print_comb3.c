099f9208e569ab948e0f6057516083cda2e8a2ca
abdul1ah33 <study.abdullah2005@gmail.com>
2023-08-12 17:15:11 -0700

    combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -21,11 +21,11 @@ int main(void)
 			{
 				putchar(digit1 + 48);
 				putchar(digit2 + 48);
-			}
-			if (digit1 + digit2 != 17)
-			{
-				putchar(',');
-				putchar(' ');
+				if (digit1 + digit2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 			digit2++;
 		}
