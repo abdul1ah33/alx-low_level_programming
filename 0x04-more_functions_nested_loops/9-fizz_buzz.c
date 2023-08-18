630322196e87715f2b935d98b3f47cee040e6fb1
abdul1ah33 <study.abdullah2005@gmail.com>
2023-08-18 00:43:34 -0700

    a

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * main - Entry point 1 - 100 followed by new line
+ *	numbers  multible by 3 print fizz
+ *	numbers  multible by 5 print buzz
+ *	numbers  multible by 3 and 5 print fizzbuzz
+ *	sparated by space
+ *
+ * Return: Always 0 (success)
+*/
+
+int main (void)
+{
+	int num;
+
+	for (num = 0; num <= 100; num++)
+	{
+		if ((num == 3) && !(num == 5))
+			printf("Fizz");
+		else if ((num == 5) && !(num == 3))
+			printf("Buzz");
+		else if ((num == 3) && (num == 5))
+			printf("FizzBuzz");
+		else
+			printf("%d", num);
+		if (num != 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+
+	return (0);
+}
