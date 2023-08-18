7443f1ee71a2fcdcf748f27673e5a38cd54c3c68
abdul1ah33 <study.abdullah2005@gmail.com>
2023-08-18 00:58:19 -0700

    a

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,10 +2,10 @@
 
 /**
  * main - Entry point 1 - 100 followed by new line
- *	numbers  multible by 3 print fizz
- *	numbers  multible by 5 print buzz
- *	numbers  multible by 3 and 5 print fizzbuzz
- *	sparated by space
+ *	numbers  that are multibles of 3 print fizz
+ *	numbers  that are multibles of 5 print buzz
+ *	numbers  that are multibles of 3 and 5 print fizzbuzz
+ *	each number and word to be separated by space
  *
  * Return: Always 0 (success)
 */
@@ -16,9 +16,9 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if ((num % 3 == 0) && !(num % 5 == 0))
+		if (num % 3 == 0 && !(num % 5 == 0))
 			printf("Fizz");
-		else if ((num % 3 == 0) && !(num % 5 == 0))
+		else if (num % 5 == 0 && !(num % 3 == 0))
 			printf("Buzz");
 		else if (num % 3 == 0 && num % 5 == 0)
 			printf("FizzBuzz");
