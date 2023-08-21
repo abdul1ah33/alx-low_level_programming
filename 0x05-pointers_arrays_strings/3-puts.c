c940d3a73d223e203e7b66b1171c824084706e1d
abdul1ah33 <study.abdullah2005@gmail.com>
2023-08-21 14:12:58 -0700

    a

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - print a string
+ *
+ * @str: takes input parameter
+ *
+ * Return: string
+*/
+
+void _puts(char *str)
+{
+	while (str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
