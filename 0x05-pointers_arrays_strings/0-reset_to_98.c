c5704aa7bf38ffdb6bdcac3cfd4a918536a75cca
abdul1ah33 <study.abdullah2005@gmail.com>
2023-08-21 10:44:14 -0700

    a

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates pointer value to 98
+ *
+ * @n: int parameter
+ *
+ * Return: nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * _putchar - write the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ *	on error, -1 is returned, and errno is set appropri
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
