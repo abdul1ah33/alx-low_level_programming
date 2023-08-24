a487c84ede36056dcb30c44c2534c69cc5582197
abdul1ah33 <study.abdullah2005@gmail.com>
2023-08-23 21:33:07 -0700

    a

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -17,9 +17,10 @@ char *_strcat(char *dest, char *src)
 	while (dest[c])
 		c++;
 
-	for (c2 = 0; src[c2], c2++)
+	for (c2 = 0; src[c2]; c2++)
 	{
-		dest[c++] = src[c2];
+		dest[c] = src[c2];
+		c++;
 	}
 
 	return (dest);
