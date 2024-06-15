721d6cca8307419ce696e6bd3e7ede842092d830
abdul1ah33 <study.abdullah2005@gmail.com>
2024-06-15 11:43:47 -0700

    hello

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,19 +6,19 @@
  */
 void rev_string(char *s)
 {
-    int lenl, len = 0, n, temp;
+	int lenl, len = 0, n, temp;
 
-    while (s[len] != '\0')
-    {
-        len++;
-    }
+	while (s[len] != '\0')
+	{
+		len++;
+	}
 
-    lenl = len - 1;
-    
-    for (n = 0; n < len / 2; n++)
-    {
-        temp = s[n];
-        s[n] = s[lenl];
-        s[lenl--] = temp;
-    }
+	lenl = len - 1;
+   
+	for (n = 0; n < len / 2; n++)
+	{
+		temp = s[n];
+		s[n] = s[lenl];
+		s[lenl--] = temp;
+	}
 }
